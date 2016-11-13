fab6584d19316480afba74fd67fb30eb9f772327
vince <vince@Debidual.lan>
2016-11-13 21:52:50 +0100

    beforeREVELATION

--- src/main.c
+++ src/main.c
@@ -6,25 +6,28 @@ int	main(int ac, char **av)
 {
 	(void)ac;
 	(void)av;
-//	ft_printf("Hello world! %s et test %");
-//	ft_printf("zz %s a %d#-LLd %% hey", "test");
+	//	ft_printf("Hello world! %s et test %");
+	//	ft_printf("zz %s a %d#-LLd %% hey", "test");
 
 
 
-//	ft_printf("zz %s a %.2d#-LLd %hey");
-//	ft_printf("%x %X %p %20.15d\n");
+	//	ft_printf("zz %s a %.2d#-LLd %hey");
+	//	ft_printf("%x %X %p %20.15d\n");
 
-//	ft_printf("\n\n|%l|\n\n",130);
+	//	ft_printf("\n\n|%l|\n\n",130);
 
 
 	ft_printf("%-10d% d%+d%010d%hhd\n");
 	ft_printf("%-10d % d%   dqd dqw d%010d%hhd\n");
 
+
+
+
 	//ft_printf("%-10d% d %+d%010d %hhd\n");
 
 
-//	ft_printf("%jd %zd %u %o %#08x\n");
-//	ft_printf("%x %#X %S %s%s\n");
+	//	ft_printf("%jd %zd %u %o %#08x\n");
+	//	ft_printf("%x %#X %S %s%s\n");
 
 
 
